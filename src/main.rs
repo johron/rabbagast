4e8470a7ae5d36c210a1fc3f47d82cea16c3a0df
Johan Rong <johanhrong@icloud.com>
2026-09-22 09:16:29 +0200

    Testing out gtk4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk4::prelude::*;
-use gtk4::{Application, ApplicationWindow, Label};
+use gtk4::{Application, ApplicationWindow, Label, Button};
 use gtk4_layer_shell::{Layer, LayerShell};
 
 fn main() {
@@ -19,10 +19,35 @@ fn main() {
 
         window.auto_exclusive_zone_enable();
 
+        let window_container = gtk4::Box::new(gtk4::Orientation::Horizontal, 0);
+        window_container.set_hexpand(true);
+        window.set_child(Some(&window_container));
+
+        let left_container = gtk4::Box::new(gtk4::Orientation::Horizontal, 5);
+        window_container.append(&left_container);
+        left_container.set_hexpand(true);
+        left_container.set_halign(gtk4::Align::Start);
+        
+        let center_container = gtk4::Box::new(gtk4::Orientation::Horizontal, 5);
+        window_container.append(&center_container);
+        center_container.set_hexpand(true);
+        center_container.set_halign(gtk4::Align::Center);
+        
+        let right_container = gtk4::Box::new(gtk4::Orientation::Horizontal, 5);
+        window_container.append(&right_container);
+        right_container.set_hexpand(true);
+        right_container.set_halign(gtk4::Align::End);
+
+        let ws_button = Button::with_label("Test");
+        left_container.append(&ws_button);
+
         let label = Label::new(Some("Du er ein ordentleg rabbagast asso!"));
         label.set_margin_top(8);
         label.set_margin_bottom(8);
-        window.set_child(Some(&label));
+        center_container.append(&label);
+
+        let pill_button = Button::with_label("Test");
+        right_container.append(&pill_button);
 
         window.present();
     });
